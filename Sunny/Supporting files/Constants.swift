a2eeede234b5a5c264a20017236bfa1c783e5776
Dmitry <borodulkin_d@mail.ru>
2022-07-28 16:43:48 +0300

    Initial Commit

---
+++ Sunny/Supporting files/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Sunny
+//
+//  Created by Дмитрий Бородулькин on 28.07.2022.
+//  Copyright © 2022 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "bd1d0570520c1ce1a231dd6b1261370e"
